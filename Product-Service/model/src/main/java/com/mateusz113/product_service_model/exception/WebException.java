b2324e5b5ae58440766157e2289289179efa28fa
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-04-30 18:52:57 +0200

    Added exceptions

---
+++ Product-Service/model/src/main/java/com/mateusz113/product_service_model/exception/WebException.java
@@ -0,0 +1,17 @@
+package com.mateusz113.product_service_model.exception;
+
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+public abstract class WebException extends RuntimeException {
+    private final int statusCode;
+    private final OffsetDateTime errorTime;
+
+    public WebException(String message, int statusCode, OffsetDateTime errorTime) {
+        super(message);
+        this.statusCode = statusCode;
+        this.errorTime = errorTime;
+    }
+}
