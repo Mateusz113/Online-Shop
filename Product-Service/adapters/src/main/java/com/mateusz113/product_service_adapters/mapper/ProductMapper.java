3be9875e34837930654e343ffc734a0d3e4d5342
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-03 23:37:04 +0200

    Update ProductMapper.java
    Added product detail mapping and detailed dto map

--- Product-Service/adapters/src/main/java/com/mateusz113/product_service_adapters/mapper/ProductMapper.java
+++ Product-Service/adapters/src/main/java/com/mateusz113/product_service_adapters/mapper/ProductMapper.java
@@ -1,10 +1,13 @@
 package com.mateusz113.product_service_adapters.mapper;
 
 import com.mateusz113.product_service_adapters.entity.CustomizationElementEntity;
+import com.mateusz113.product_service_adapters.entity.ProductDetailEntity;
 import com.mateusz113.product_service_adapters.entity.ProductEntity;
 import com.mateusz113.product_service_model.customization.CustomizationElement;
 import com.mateusz113.product_service_model.product.Product;
+import com.mateusz113.product_service_model.product.ProductDetail;
 import com.mateusz113.product_service_model_public.commands.UpsertProductCommand;
+import com.mateusz113.product_service_model_public.dto.DetailedProductDto;
 import com.mateusz113.product_service_model_public.dto.ProductDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -20,12 +23,19 @@ public interface ProductMapper {
 
     ProductDto modelToDto(Product product);
 
+    DetailedProductDto modelToDetailedDto(Product product);
+
     List<ProductDto> modelListToDtoList(List<Product> products);
 
     @Mapping(target = "id", ignore = true)
     Product commandToModel(UpsertProductCommand upsertProductCommand);
 
     List<Product> commandListToModelList(List<UpsertProductCommand> upsertProductCommands);
+
+    default ProductDetailEntity productDetailModelToEntity(ProductDetail productDetail) {
+        ProductDetailMapper mapper = Mappers.getMapper(ProductDetailMapper.class);
+        return mapper.modelToEntity(productDetail);
+    }
 
     default CustomizationElementEntity customizationElementModelToEntity(CustomizationElement customizationElement) {
         CustomizationElementMapper mapper = Mappers.getMapper(CustomizationElementMapper.class);
