e6af02ee598ed8835830a60f027a2678822153bb
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-03 23:39:50 +0200

    Update ProductServiceDatabase.java
    Added filter

--- Product-Service/core/src/main/java/com/mateusz113/product_service_core/ports/outgoing/ProductServiceDatabase.java
+++ Product-Service/core/src/main/java/com/mateusz113/product_service_core/ports/outgoing/ProductServiceDatabase.java
@@ -1,6 +1,7 @@
 package com.mateusz113.product_service_core.ports.outgoing;
 
 import com.mateusz113.product_service_model.content_management.PageableContent;
+import com.mateusz113.product_service_model.filter.ProductFilter;
 import com.mateusz113.product_service_model.product.Product;
 
 import java.util.Optional;
@@ -8,11 +9,9 @@ import java.util.Optional;
 public interface ProductServiceDatabase {
     Product save(Product product);
 
-    void delete(Product product);
-
     Optional<Product> findById(Long productId);
 
-    PageableContent<Product> findAll(int pageNumber, int pageSize);
+    PageableContent<Product> findAll(ProductFilter filter, int pageNumber, int pageSize);
 
-    PageableContent<Product> findByType(String type, int pageNumber, int pageSize);
+    void delete(Product product);
 }
