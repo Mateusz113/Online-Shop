771840ff907a4cc4e0f683a92870928c507da84c
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-03 23:37:18 +0200

    Update ProductEntityRepository.java
    Removed redundant function

--- Product-Service/adapters/src/main/java/com/mateusz113/product_service_adapters/repository/ProductEntityRepository.java
+++ Product-Service/adapters/src/main/java/com/mateusz113/product_service_adapters/repository/ProductEntityRepository.java
@@ -1,12 +1,10 @@
 package com.mateusz113.product_service_adapters.repository;
 
 import com.mateusz113.product_service_adapters.entity.ProductEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductEntityRepository extends JpaRepository<ProductEntity, Long> {
-    Page<ProductEntity> findByType(String type, Pageable pageable);
+public interface ProductEntityRepository extends JpaRepository<ProductEntity, Long>, JpaSpecificationExecutor<ProductEntity> {
 }
