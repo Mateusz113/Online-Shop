fc2b68aea5fe7890356962540dfa38f891cb7ba5
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-03 23:36:21 +0200

    Update ProductServiceDatabaseAdapter.java

--- Product-Service/adapters/src/main/java/com/mateusz113/product_service_adapters/adapter/ProductServiceDatabaseAdapter.java
+++ Product-Service/adapters/src/main/java/com/mateusz113/product_service_adapters/adapter/ProductServiceDatabaseAdapter.java
@@ -5,12 +5,17 @@ import com.mateusz113.product_service_adapters.mapper.ProductMapper;
 import com.mateusz113.product_service_adapters.repository.ProductEntityRepository;
 import com.mateusz113.product_service_core.ports.outgoing.ProductServiceDatabase;
 import com.mateusz113.product_service_model.content_management.PageableContent;
+import com.mateusz113.product_service_model.filter.ProductFilter;
 import com.mateusz113.product_service_model.product.Product;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.Optional;
+
+import static com.mateusz113.product_service_adapters.specification.ProductSpecification.getSpecificationFromFilter;
 
 @RequiredArgsConstructor
 public class ProductServiceDatabaseAdapter implements ProductServiceDatabase {
@@ -18,16 +23,11 @@ public class ProductServiceDatabaseAdapter implements ProductServiceDatabase {
     private final ProductMapper mapper;
 
     @Override
+    @Transactional
     public Product save(Product product) {
         ProductEntity entity = mapper.modelToEntity(product);
         entity = repository.save(entity);
         return mapper.entityToModel(entity);
-    }
-
-    @Override
-    public void delete(Product product) {
-        ProductEntity entity = mapper.modelToEntity(product);
-        repository.delete(entity);
     }
 
     @Override
@@ -37,26 +37,22 @@ public class ProductServiceDatabaseAdapter implements ProductServiceDatabase {
     }
 
     @Override
-    public PageableContent<Product> findAll(int pageNumber, int pageSize) {
-        Page<ProductEntity> entities = repository.findAll(PageRequest.of(pageNumber, pageSize));
+    public PageableContent<Product> findAll(ProductFilter filter, int pageNumber, int pageSize) {
+        Specification<ProductEntity> specification = getSpecificationFromFilter(filter);
+        Page<ProductEntity> entityPage = repository.findAll(specification, PageRequest.of(pageNumber, pageSize));
         return PageableContent.<Product>builder()
-                .totalElements(entities.getTotalElements())
-                .totalPages(entities.getTotalPages())
+                .totalElements(entityPage.getTotalElements())
+                .totalPages(entityPage.getTotalPages())
                 .pageNumber(pageNumber)
                 .pageSize(pageSize)
-                .elements(entities.get().map(mapper::entityToModel).toList())
+                .elements(entityPage.get().map(mapper::entityToModel).toList())
                 .build();
     }
 
     @Override
-    public PageableContent<Product> findByType(String type, int pageNumber, int pageSize) {
-        Page<ProductEntity> entities = repository.findByType(type, PageRequest.of(pageNumber, pageSize));
-        return PageableContent.<Product>builder()
-                .totalElements(entities.getTotalElements())
-                .totalPages(entities.getTotalPages())
-                .pageNumber(pageNumber)
-                .pageSize(pageSize)
-                .elements(entities.get().map(mapper::entityToModel).toList())
-                .build();
+    @Transactional
+    public void delete(Product product) {
+        ProductEntity entity = mapper.modelToEntity(product);
+        repository.delete(entity);
     }
 }
