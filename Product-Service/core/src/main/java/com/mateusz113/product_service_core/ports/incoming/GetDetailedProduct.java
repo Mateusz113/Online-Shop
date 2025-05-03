f7df0c265b1b9c923fbc5353762de9ba585f81b8
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-03 23:29:23 +0200

    Updated port name

--- Product-Service/core/src/main/java/com/mateusz113/product_service_core/ports/incoming/GetDetailedProduct.java
+++ Product-Service/core/src/main/java/com/mateusz113/product_service_core/ports/incoming/GetDetailedProduct.java
@@ -2,6 +2,6 @@ package com.mateusz113.product_service_core.ports.incoming;
 
 import com.mateusz113.product_service_model.product.Product;
 
-public interface GetProduct {
+public interface GetDetailedProduct {
     Product getById(Long productId);
 }
