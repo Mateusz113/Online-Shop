e60d032dca169e50a527785bc706ae6623311936
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-03 23:41:34 +0200

    Update ProductServiceFacade.java
    Removed reference fixing
    fixed the crud order of methods
    added id verification
    fixed the getAll to get filter

--- Product-Service/core/src/main/java/com/mateusz113/product_service_core/ProductServiceFacade.java
+++ Product-Service/core/src/main/java/com/mateusz113/product_service_core/ProductServiceFacade.java
@@ -2,12 +2,13 @@ package com.mateusz113.product_service_core;
 
 import com.mateusz113.product_service_core.ports.incoming.AddNewProducts;
 import com.mateusz113.product_service_core.ports.incoming.DeleteProduct;
-import com.mateusz113.product_service_core.ports.incoming.GetProduct;
+import com.mateusz113.product_service_core.ports.incoming.GetDetailedProduct;
 import com.mateusz113.product_service_core.ports.incoming.GetProducts;
 import com.mateusz113.product_service_core.ports.incoming.UpdateProduct;
 import com.mateusz113.product_service_core.ports.outgoing.ProductServiceDatabase;
 import com.mateusz113.product_service_model.content_management.PageableContent;
 import com.mateusz113.product_service_model.exception.ProductDoesNotExistException;
+import com.mateusz113.product_service_model.filter.ProductFilter;
 import com.mateusz113.product_service_model.product.Product;
 import lombok.RequiredArgsConstructor;
 
@@ -17,41 +18,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
-public class ProductServiceFacade implements AddNewProducts, DeleteProduct, GetProduct, GetProducts, UpdateProduct {
+public class ProductServiceFacade implements AddNewProducts, DeleteProduct, GetDetailedProduct, GetProducts, UpdateProduct {
     private final ProductServiceDatabase database;
     private final ProductServiceVerifier verifier;
     private final Clock clock;
 
     @Override
     public List<Product> add(List<Product> products) {
+        verifier.verifyProductList(products);
         List<Product> savedProducts = new ArrayList<>();
         for (Product product : products) {
-            fixProductInternalReferences(product);
-            verifier.verifyProduct(product);
             savedProducts.add(database.save(product));
         }
         return savedProducts;
     }
 
     @Override
-    public void delete(Long productId) {
-        Product product = findProductById(productId);
-        database.delete(product);
-    }
-
-    @Override
     public Product getById(Long productId) {
+        verifier.verifyProductId(productId);
         return findProductById(productId);
     }
 
     @Override
-    public PageableContent<Product> getAll(int pageNumber, int pageSize) {
-        return database.findAll(pageNumber, pageSize);
-    }
-
-    @Override
-    public PageableContent<Product> getByType(String type, int pageNumber, int pageSize) {
-        return database.findByType(type, pageNumber, pageSize);
+    public PageableContent<Product> getAll(ProductFilter filter, int pageNumber, int pageSize) {
+        verifier.verifyProductFilter(filter);
+        return database.findAll(filter, pageNumber, pageSize);
     }
 
     @Override
@@ -62,15 +53,15 @@ public class ProductServiceFacade implements AddNewProducts, DeleteProduct, GetP
         database.save(product);
     }
 
+    @Override
+    public void delete(Long productId) {
+        verifier.verifyProductId(productId);
+        Product product = findProductById(productId);
+        database.delete(product);
+    }
+
     private Product findProductById(Long productId) {
         return database.findById(productId)
                 .orElseThrow(() -> new ProductDoesNotExistException("Product with given ID does not exist.", OffsetDateTime.now(clock)));
-    }
-
-    private void fixProductInternalReferences(Product product) {
-        product.getCustomizations().forEach(customization -> {
-            customization.setProduct(product);
-            customization.getOptions().forEach(option -> option.setCustomizationElement(customization));
-        });
     }
 }
