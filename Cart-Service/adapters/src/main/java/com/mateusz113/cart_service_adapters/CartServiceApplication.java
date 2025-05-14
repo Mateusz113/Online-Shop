9053e3b5e9db17b6765704826eb081b9462b79ce
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-14 15:02:10 +0200

    Update CartServiceApplication.java
    Added scheduling

--- Cart-Service/adapters/src/main/java/com/mateusz113/cart_service_adapters/CartServiceApplication.java
+++ Cart-Service/adapters/src/main/java/com/mateusz113/cart_service_adapters/CartServiceApplication.java
@@ -3,9 +3,11 @@ package com.mateusz113.cart_service_adapters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class CartServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartServiceApplication.class, args);
