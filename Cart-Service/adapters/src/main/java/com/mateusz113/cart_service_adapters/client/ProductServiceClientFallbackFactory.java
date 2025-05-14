e6a1b225b90912358dde62aa49461cc5968dae60
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-14 15:02:21 +0200

    Update ProductServiceClientFallbackFactory.java
    Updated the fallback logic

--- Cart-Service/adapters/src/main/java/com/mateusz113/cart_service_adapters/client/ProductServiceClientFallbackFactory.java
+++ Cart-Service/adapters/src/main/java/com/mateusz113/cart_service_adapters/client/ProductServiceClientFallbackFactory.java
@@ -1,6 +1,7 @@
 package com.mateusz113.cart_service_adapters.client;
 
 import com.mateusz113.cart_service_model.exception.ProductServiceCommunicationErrorException;
+import com.mateusz113.cart_service_model.exception.ProductServiceIsUnavailableException;
 import com.mateusz113.cart_service_model.exception.SourceProductNotPresentException;
 import com.mateusz113.cart_service_model.product.SourceProduct;
 import feign.FeignException;
@@ -23,11 +24,12 @@ public class ProductServiceClientFallbackFactory implements FallbackFactory<Prod
             public SourceProduct getProductById(Long id) {
                 if (cause instanceof FeignException exception) {
                     if (exception.status() == 404) {
-                        throw new SourceProductNotPresentException("Source product with id: %d does not exist.", OffsetDateTime.now(clock));
+                        throw new SourceProductNotPresentException("Source product with id: %d does not exist.".formatted(id), OffsetDateTime.now(clock));
+                    } else if (exception.status() == 503) {
+                        throw new ProductServiceIsUnavailableException("Product service is currently unavailable.", OffsetDateTime.now(clock));
                     }
-                    throw new ProductServiceCommunicationErrorException("Could not communicate with service to get source data.", OffsetDateTime.now(clock));
                 }
-                return null;
+                throw new ProductServiceCommunicationErrorException("Could not communicate with service to get source data.", OffsetDateTime.now(clock));
             }
         };
     }
