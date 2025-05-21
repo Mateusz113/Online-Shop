9f4fd9aeca886478c72abac3891912cabddd0dff
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-21 22:16:18 +0200

    Update ProductServiceDatabase.java

--- Product-Service/core/src/main/java/com/mateusz113/product_service_core/ports/outgoing/ProductServiceDatabase.java
+++ Product-Service/core/src/main/java/com/mateusz113/product_service_core/ports/outgoing/ProductServiceDatabase.java
@@ -4,12 +4,17 @@ import com.mateusz113.product_service_model.content_management.PageableContent;
 import com.mateusz113.product_service_model.filter.ProductFilter;
 import com.mateusz113.product_service_model.product.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductServiceDatabase {
     Product save(Product product);
 
+    void saveAll(List<Product> products);
+
     Optional<Product> findById(Long productId);
+
+    List<Product> findAllByIds(List<Long> ids);
 
     PageableContent<Product> findAll(ProductFilter filter, int pageNumber, int pageSize);
 
