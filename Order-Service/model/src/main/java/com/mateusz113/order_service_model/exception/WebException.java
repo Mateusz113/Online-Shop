e0d43f88312b1c64ae2f695fef254f0a9e76c051
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-23 14:27:13 +0200

    Update WebException.java
    Fixed initialization

--- Order-Service/model/src/main/java/com/mateusz113/order_service_model/exception/WebException.java
+++ Order-Service/model/src/main/java/com/mateusz113/order_service_model/exception/WebException.java
@@ -10,6 +10,7 @@ public abstract class WebException extends RuntimeException {
     private final OffsetDateTime errorTime;
 
     public WebException(String message, int statusCode, OffsetDateTime errorTime) {
+        super(message);
         this.statusCode = statusCode;
         this.errorTime = errorTime;
     }
