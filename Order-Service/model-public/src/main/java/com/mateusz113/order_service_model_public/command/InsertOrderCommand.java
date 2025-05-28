398940beca2a28c71386fbc10113bcc493eadb45
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-29 01:53:53 +0200

    Added builder

--- Order-Service/model-public/src/main/java/com/mateusz113/order_service_model_public/command/InsertOrderCommand.java
+++ Order-Service/model-public/src/main/java/com/mateusz113/order_service_model_public/command/InsertOrderCommand.java
@@ -1,5 +1,8 @@
 package com.mateusz113.order_service_model_public.command;
 
+import lombok.Builder;
+
+@Builder
 public record InsertOrderCommand(
         Long cartId,
         Long clientId,
--- Order-Service/model-public/src/main/java/com/mateusz113/order_service_model_public/command/UpdateOrderStatusCommand.java
+++ Order-Service/model-public/src/main/java/com/mateusz113/order_service_model_public/command/UpdateOrderStatusCommand.java
@@ -1,7 +1,9 @@
 package com.mateusz113.order_service_model_public.command;
 
 import com.mateusz113.order_service_model_public.dto.ClientDto;
+import lombok.Builder;
 
+@Builder
 public record UpdateOrderStatusCommand(
         Long orderId,
         ClientDto clientDto
