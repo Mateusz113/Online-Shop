e05b0a75a93406bbd1868f283aec2d2a793960a8
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-29 12:31:11 +0200

    Update ProductServiceClientFallbackFactory.java

--- Order-Service/adapters/src/main/java/com/mateusz113/order_service_adapters/fallback/ProductServiceClientFallbackFactory.java
+++ Order-Service/adapters/src/main/java/com/mateusz113/order_service_adapters/fallback/ProductServiceClientFallbackFactory.java
@@ -4,17 +4,15 @@ import com.mateusz113.order_service_adapters.client.ProductServiceClient;
 import com.mateusz113.order_service_model.exception.ProductInsufficientStockException;
 import com.mateusz113.order_service_model.exception.ProductNotInStockException;
 import com.mateusz113.order_service_model.exception.ServiceCommunicationErrorException;
-import com.mateusz113.order_service_model_public.dto.ProductAvailabilityDto;
+import com.mateusz113.order_service_model_public.command.UpdateProductsStocksCommand;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
-import okhttp3.Response;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.time.Clock;
 import java.time.OffsetDateTime;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -38,7 +36,7 @@ public class ProductServiceClientFallbackFactory implements FallbackFactory<Prod
             }
 
             @Override
-            public void updateSoldProductsStock(Map<Long, Integer> productsStockMap) {
+            public void updateSoldProductsStock(UpdateProductsStocksCommand command) {
                 if (cause instanceof FeignException exception) {
                     if (exception.status() == 404) {
                         throw new ProductNotInStockException("Could not update products stock as they are not present.", OffsetDateTime.now(clock));
