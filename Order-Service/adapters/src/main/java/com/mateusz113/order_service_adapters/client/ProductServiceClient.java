182d1afda9976d5be28619d6c80afcef0e050f75
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-05-29 02:02:14 +0200

    Update ProductServiceClient.java
    ResponseEntity is now returned due to feign complications

--- Order-Service/adapters/src/main/java/com/mateusz113/order_service_adapters/client/ProductServiceClient.java
+++ Order-Service/adapters/src/main/java/com/mateusz113/order_service_adapters/client/ProductServiceClient.java
@@ -2,11 +2,14 @@ package com.mateusz113.order_service_adapters.client;
 
 import com.mateusz113.order_service_adapters.config.FeignConfig;
 import com.mateusz113.order_service_adapters.fallback.ProductServiceClientFallbackFactory;
-import feign.Response;
+import okhttp3.Response;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -17,8 +20,8 @@ import java.util.Map;
         fallbackFactory = ProductServiceClientFallbackFactory.class
 )
 public interface ProductServiceClient {
-    @GetMapping("/products/available-amount")
-    Response checkProductsAvailability(@RequestBody Map<Long, Integer> productStockMap);
+    @GetMapping("/products/{productId}/available-amount")
+    ResponseEntity<Void> checkProductsAvailability(@PathVariable Long productId, @RequestParam Integer requiredStock);
 
     @PatchMapping("/products/available-amount")
     void updateSoldProductsStock(@RequestBody Map<Long, Integer> productsStockMap);
